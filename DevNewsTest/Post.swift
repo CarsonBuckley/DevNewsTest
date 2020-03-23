4da68ce92b7f3ae54718acbab24b869f2ac7a42f
Carson <carsonbuckley808@gmail.com>
2020-03-23 00:00:36 -0600

    Initial commit, create project

---
+++ DevNewsTest/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  DevNewsTest
+//
+//  Created by Carson Buckley on 3/22/20.
+//  Copyright © 2020 Carson Buckley. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    
+    var title: String
+    var subText: String
+    var link: String?
+    var category: String
+    
+}
